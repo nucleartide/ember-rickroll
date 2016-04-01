af76fb86088bba7f4c55ae9ff2cd03b18dbe2e25
Jason Tu <jasontu4@gmail.com>
2016-04-01 15:29:42 -0400

    Update component.js

--- addon/components/rick-roll/component.js
+++ addon/components/rick-roll/component.js
@@ -1,3 +1,4 @@
+
 import Ember from 'ember'
 import layout from './template'
 import SC from 'soundcloud'
@@ -7,8 +8,8 @@ const RICKROLL_URL = 'https://soundcloud.com/catsystemcorp/nggyu-3'
 const IFRAME_SRC = `https://w.soundcloud.com/player?url=${RICKROLL_URL}`
 
 export default Ember.Component.extend({
-  layout,
   tagName: 'iframe',
+  layout,
 
   attributeBindings: ['src', 'style'],
   src: IFRAME_SRC,
